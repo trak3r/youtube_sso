7d6454e66babbb913699f7fb294c884bddadd153
Teflon Ted <github@rudiment.net>
2009-01-12 18:50:30 -0500

    first commit

---
+++ youtube_sso.rb
@@ -0,0 +1,29 @@
+require 'net/http'
+require 'uri'
+require 'rubygems'
+require 'hpricot'
+
+def valid?(username, password)
+  response = Net::HTTP.post_form(URI.parse('http://www.youtube.com/login'),
+    {
+      'username' => username,
+      'password' => password,
+      'current_form' => 'loginForm',
+      'next' => '/',
+      'action_login' => 'Log+In'
+    })
+  page = Hpricot(response.body)
+  denial = page.search("div[@class='errorBox']")
+  acceptance = page.search("a[@class='noul']")
+  if denial.empty?
+    if acceptance.empty?
+      return page # fubar
+    else
+      return true
+    end
+  else
+    return false
+  end
+end
+
+pp valid?(ARGV[0], ARGV[1])
