45ade063b5ef7379c1907d4a595225e2acbb9bf7
Teflon Ted <github@rudiment.net>
2009-01-13 08:46:03 -0500

    ah it's finally working

--- youtube_sso.rb
+++ youtube_sso.rb
@@ -6,22 +6,23 @@ require 'hpricot'
 class YouTubeSSO
   class << self
     def valid?(username, password)
+      target = 'sso'
       response = Net::HTTP.post_form(URI.parse('http://www.youtube.com/login'),
         {
           'username' => username,
           'password' => password,
           'current_form' => 'loginForm',
-          'next' => '/',
+          'next' => "/#{target}",
           'action_login' => 'Log+In'
         })
       page = Hpricot(response.body)
       denial = page.search("div[@class='errorBox']")
       acceptance = page.search("a[@class='noul']")
       if denial.empty?
-        if acceptance.empty?
-          return page # fubar
-        else
+        if "http://www.youtube.com/#{target}" == response['location']
           return true
+        else
+          return response # for debugging
         end
       else
         return false
